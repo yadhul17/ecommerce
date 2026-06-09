92a919c7e6f2d74d373eee4d729d38406fa22f2a
yadhul17 <yadhul jayakumar@gmail.com>
2026-06-09 22:12:46 +0530

    add STATIC_ROOT

--- aura/aura/settings.py
+++ aura/aura/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'                        # URL prefix for media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
