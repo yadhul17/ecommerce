8bd9277dbd4b9f2c476f5132dc600109a7b63bc5
yadhul17 <yadhul jayakumar@gmail.com>
2026-06-09 23:48:20 +0530

    settings updated

--- aura/aura/settings.py
+++ aura/aura/settings.py
@@ -144,6 +144,11 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+)
 
 
 
