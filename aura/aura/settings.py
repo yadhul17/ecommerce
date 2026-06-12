5535a7c028120d79df164c50be6ef983a9355f3c
yadhul17 <yadhul jayakumar@gmail.com>
2026-06-12 17:25:10 +0530

    view updated

--- aura/aura/settings.py
+++ aura/aura/settings.py
@@ -166,8 +166,11 @@ cloudinary.config(
 
 
 
+
+# settings.py
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  
 
 
 MEDIA_URL = '/media/'
