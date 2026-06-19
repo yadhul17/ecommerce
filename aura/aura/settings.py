8b4a7b51579b1135b354091914ad48a0c820521d
yadhul17 <yadhul jayakumar@gmail.com>
2026-06-19 14:03:33 +0530

    settings updated

--- aura/aura/settings.py
+++ aura/aura/settings.py
@@ -197,6 +197,7 @@ EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+EMAIL_TIMEOUT = 10
 
 import razorpay
 RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
